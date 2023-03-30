db1a38d4f56964c9b25989f8092f1f90a691889f
GJQKP <126954358+GJQKP@users.noreply.github.com>
2023-03-30 14:03:34 +0000

    Add files via upload

---
+++ strings/helpers.py
@@ -0,0 +1,167 @@
+
+HELP_1 = """✅**<u>اوامر الادمنيه:</u>**
+
+**c** اوامر الموسيقى.
+
+/ايقاف- لإيقاف الموسيقى مؤقتاً
+/استمر- لإستمرار الموسيقى
+/اسكت- لكتم الموسيقى.
+/اتكلم- لإلغاء كتم الموسيقى.
+/تخطي- لتخطي الموسيقى.
+/انهاء- لإنهاء الموسيقى.
+/خلط- ترتيب عشوائي لقائمة التشغيل في قائمة الانتظار.
+/بحث- للبحث عن موسيقى
+/الغاء بحث- لإلغاء بحث عن الموسيقى
+/restart - Restart bot for your chat .
+
+
+✅<u>**تخطي محدد**</u>
+/تخطي [Number(example: 3)] 
+    - يتخطى الموسيقى إلى الرقم المحدد في قائمة الانتظار. مثال: سيتخطى / skip 3 الموسيقى إلى الموسيقى في قائمة الانتظار الثالثة ويتجاهل الموسيقى 1 و 2 في قائمة الانتظار.
+
+✅<u>**حلقة التشغيل:**</u>
+/حلقة [enable/disable] or [Numbers between 1-10] 
+    -عند التنشيط ، يقوم البوت بتكرار تشغيل الموسيقى الحالية إلى 1-10 مرات في الدردشة الصوتية. افتراضي إلى 10 مرات.
+
+✅<u>**المسؤولون:**</u>
+يمكن لمستخدمي المصادقة استخدام أوامر المسؤول بدون حقوق المسؤول في الدردشة.
+
+/رفع مسؤول [Username] - تمت الإضافة إلى قائمة المستخدمين المعتمدين لمجموعتك.
+/تنزيل مسؤول [Username] - تمت إزالته من قائمة المستخدمين المعتمدين لهذه المجموعة.
+/المسئولون - Check AUTH LIST of the group."""
+
+
+HELP_2 = """✅<u>**اوامر التشغيل:**</u>
+
+Available Commands = تشغيل , تشغيل فيديو , cplay
+
+ForcePlay Commands = playforce , vplayforce , cplayforce
+
+**c** قناة تشغيل الموسيقى.
+**v** قناة تشغيل الفيديوهات.
+**force** stands for force play.
+
+/تشغيل or /تشغيل فيديو or /cplay  - سيبدأ البوت في تشغيل استعلامك المحدد في الدردشة الصوتية أو بث الروابط المباشرة على الدردشات الصوتية.
+
+/playforce or /vplayforce or /cplayforce -  **Force Play** stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
+
+/channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
+
+
+✅**<u>سيرڤر قائمة التشغيل:</u>**
+/قائمة التشغيل  - لفحص قائمة التشغيل.
+/حذف قائمة التشغيل - لحذف قائمة التشغيل.
+/تشغيل  - لتشغيل الموسيقى."""
+
+
+HELP_3 = """✅<u>**اوامر البوت:**</u>
+
+/احصائيات - احصل على أفضل 10 مسارات للإحصائيات العالمية ، أفضل 10 مستخدمين للروبوت ، أفضل 10 محادثات على الروبوت ، أفضل 10 تم لعبها في محادثة ، إلخ.
+
+/المسؤولون الاعلى - بحث عن المسؤولون الاعلى  of Steve MUSIC
+
+/كلمات [Music Name] - يبحث في كلمات عن موسيقى معينة على الويب.
+
+/غني [Track Name] or [YT Link] - تحميل اي موسيقى in mp3 or mp4 formats.
+
+/المشغل -  احصل على لوحة موسيقى تفاعلية..
+
+**c** stands for channel play.
+
+/queue or /cqueue- Check Queue List of Music."""
+
+HELP_4 = """✅<u>**اوامر اضافيه:**</u>
+/start - Start the Music Bot.
+/help  - احصل على قائمة مساعد الأوامر مع شرح مفصل للأوامر.
+/ping- Ping the Bot and check Ram, Cpu etc stats of Bot.
+
+✅<u>**اعدادات المجموعه:**</u>
+/settings - احصل على إعدادات المجموعة الكاملة باستخدام الأزرار المضمنة
+
+🔗 **الخيارات في الاعدادات:**
+
+1️⃣ يمكنك ضبط ** جودة الصوت ** التي تريد بثها على الدردشة الصوتية.
+
+2️⃣ يمكنك ضبط ** جودة الفيديو ** التي تريد بثها على الدردشة الصوتية.
+
+3️⃣ **المسئولون**:- يمكنك تغيير وضع أوامر المسؤول من هنا للجميع أو للمسؤولين فقط. إذا كان الجميع ، أي شخص موجود في مجموعتك سيكون قادرًا على استخدام أوامر المسؤول (تشغيل / تخطي ، / إيقاف ، إلخ)
+
+4️⃣ **وضع التنظيف:** عند التمكين ، يحذف رسائل البوت بعد 5 دقائق من مجموعتك للتأكد من بقاء محادثتك نظيفة وجيدة.
+
+5️⃣ **امر التنظيف** : عند التنشيط ، سيحذف Bot أوامره المنفذة (/ play ، / pause ، / shuffle ، / stop وما إلى ذلك) على الفور.
+
+6️⃣ **اعدادات الالعاب:**
+
+/وضع الالعاب - احصل على لوحة إعدادات العاب كاملة مع أزرار حيث يمكنك ضبط إعدادات العاب مجموعتك.
+
+<u>الخيارات فوضع الالعاب:</u>
+
+1️⃣ **وضع البحث** [Direct or Inline] - Changes your search mode while you give /play mode. 
+
+2️⃣ **اوامر المسؤول** [الجميع or المسؤول] - إذا كان الجميع ، أي شخص موجود في مجموعتك سيكون قادرًا على استخدام أوامر المسؤول (تشغيل / تخطي ، / إيقاف ، إلخ)
+
+3️⃣ **التحكم في الموسيقى** [الجميع or المسؤول] - لو المسؤول فهو الوحيد القادر على التحكم في التشغيل والتخطي والايقاف اما اذا الجميع فالجميع قادر على التحكم"""
+
+HELP_5 = """🔰**<u>اضافة او حذف مطورين :</u>**
+/رفع مطور [Username or Reply to a user]
+/تنزيل مطور [Username or Reply to a user]
+
+🛃**<u>HEROKU:</u>**
+/usage - Dyno Usage.
+
+🌐**<u>CONFIG VARS:</u>**
+/get_var - Get a config var from Heroku or .env.
+/del_var - Delete any var on Heroku or .env.
+/set_var [Var Name] [Value] - Set a Var or Update a Var on heroku or .env. Seperate Var and its Value with a space.
+
+🤖**<u>BOT COMMANDS:</u>**
+/reboot - Reboot your Bot. 
+/update - Update Bot.
+/speedtest - Check server speeds
+/maintenance [enable / disable] 
+/logger [enable / disable] - Bot logs the searched queries in logger group.
+/get_log [Number of Lines] - Get log of your bot from heroku or vps. Works for both.
+/autoend [enable|disable] - Enable Auto stream end after 3 mins if no one is listening.
+
+📈**<u>STATS COMMANDS:</u>**
+/activevoice - Check active voice chats on bot.
+/activevideo - Check active video calls on bot.
+/stats - Check Bots Stats
+
+⚠️**<u>BLACKLIST CHAT FUNCTION:</u>**
+/blacklistchat [CHAT_ID] - Blacklist any chat from using Music Bot
+/whitelistchat [CHAT_ID] - Whitelist any blacklisted chat from using Music Bot
+/blacklistedchat - Check all blacklisted chats.
+
+👤**<u>BLOCKED FUNCTION:</u>**
+/block [Username or Reply to a user] - Prevents a user from using bot commands.
+/unblock [Username or Reply to a user] - Remove a user from Bot's Blocked List.
+/blockedusers - Check blocked Users Lists
+
+👤**<u>GBAN FUNCTION:</u>**
+/gban [Username or Reply to a user] - Gban a user from bot's served chat and stop him from using your bot.
+/ungban [Username or Reply to a user] - Remove a user from Bot's gbanned List and allow him for using your bot
+/gbannedusers - Check Gbanned Users Lists
+
+🎥**<u>VIDEOCALLS FUNCTION:</u>**
+/set_video_limit [Number of Chats] - Set a maximum Number of Chats allowed for Video Calls at a time. Default to 3 chats.
+/videomode [download|m3u8] - If download mode is enabled, Bot will download videos instead of playing them in M3u8 form. ByDefault to M3u8. You can use download mode when any query doesnt plays in m3u8 mode.
+
+⚡️**<u>PRIVATE BOT FUNCTION:</u>**
+/authorize [CHAT_ID] - Allow a chat for using your bot.
+/unauthorize [CHAT_ID] - Disallow a chat from using your bot.
+/authorized - Check all allowed chats of your bot.
+
+🌐**<u>BROADCAST FUNCTION:</u>**
+/broadcast [Message or Reply to a Message] - Broadcast any message to Bot's Served Chats.
+
+<u>options for broadcast:</u>
+**-pin** : This will pin your message 
+**-pinloud** : This will pin your message with loud notification
+**-user** : This will broadcast your message to the users who have started your bot.
+**-assistant** : This will broadcast your message from assistant account of your bot.
+**-nobot** : This will force your bot to not broadcast message
+
+**Example:** `/broadcast -user -assistant -pin Hello Testing`
+
+"""
---
+++ strings/readme.md
@@ -0,0 +1,18 @@
+# StarMusic Multi-Language Support
+
+- These all are the languages currently available in StarMusic Bot. You can edit or change all strings available.
+
+| Code | Language | Contributor |
+|-|-------|-------|
+| en | English | Thanks to [TeamStarMusic](https://t.me/S0URCE_STAR)
+| si | Sinhala  | Thanks to [Damantha](https://t.me/MrItzme) & [Supun](https://t.me/Supunma)
+| hi | Hindi  | Thanks to [TeamStarMusic](https://t.me/S0URCE_STAR)
+| ar | Arabic | Thanks to [ROWES](https://t.me/RQ_VX)
+| te | Telugu | Thanks to [SIMO](https://t.me/RQ_VX)
+
+
+### We need your help in translating StarMusic Bot. How to Contribute?
+
+You can edit [`en.yml`](https://github.com/DaRrkNneSs1/STAR_MUSIC/blob/master/strings/langs/en.yml) present in langs folder to your own language and send us the edited file at [@StarMusicSupport](https://t.me/StarMusicSupport)
+
+- > Points to remember while editing : <br> - Make sure you dont change any `{0}` or `{1}` while editing your codes <br> - Don’t change "general_1" or any other such keys present.
